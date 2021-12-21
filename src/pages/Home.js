9d7a8d7c43d5e1ad68766055078d14745980c35a
Pegadro <pegadro.alvarado@gmail.com>
2021-12-21 13:16:29 -0600

    Creando el Home

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    const view = `
+        <div class="Characters">
+            <articles class="Character-item">
+                <a href="#/1/">
+                    <img src="image" alt="name">
+                    <h2>Name</h2>
+                </a>
+            </article>
+        </div>
+    `;
+    return view;
+};
+
+export { Home };
